181de4c2a696133cfe4ab4e6ca3e8a6db8c578b7
Sandeep <sandeeppakho55@gmail.com>
2023-06-14 13:23:27 +0530

    FEAT : Addded New Components in Homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Homepage from './Pages/Homepage';
+import { SignupPage } from './components/Homepage/SignupPage';
 
 function App() {
   return (
     <div className="App overflow-x-hidden">
-       <Homepage/>
+       
+       <Routes>
+         <Route path='/' element={<Homepage/>}/>
+         <Route path='/signup' element={<SignupPage/>}/>
+       </Routes>
     </div>
   );
 }
