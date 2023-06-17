ac9ff53b90ded1b98b48692212f4ac16fff873cd
Sandeep <sandeeppakho55@gmail.com>
2023-06-17 23:50:37 +0530

    FEAT Login Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Homepage from './Pages/Homepage';
 import {SignupPage }from './Pages/SignupPage';
 import { Navbar } from './components/common/Navbar';
+import  PrivateRoute  from './components/Auth/PrivateRoute';
+import { Dashboard } from './Pages/Dashboard';
 
 
 function App() {
@@ -13,7 +15,13 @@ function App() {
 
         <Routes>
           <Route path='/' element={<Homepage />} />
-          <Route path='/signup' element={<SignupPage />} />
+          <Route path='/signup' element={<SignupPage isLogin={false}/>} />
+          <Route path='/login' element={<SignupPage isLogin={true}/>} />
+          <Route path='/dashboard' element={
+            <PrivateRoute>
+              <Dashboard/>
+            </PrivateRoute>
+          }/>
         </Routes>
 
         
